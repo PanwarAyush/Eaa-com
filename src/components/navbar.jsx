b9ac97b5738fd01a161bdd6692a251fcd30cb7a6
@yush P@nwar <ayushpanwar691@gmail.com>
2022-06-09 14:50:43 +0530

    Initial commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,102 @@
+import { Badge } from "@material-ui/core";
+import {Search ,ShoppingCartOutlined} from "@material-ui/icons";
+import React from 'react'
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+import { mobile } from "../responsive";
+const Container= styled.div`
+ height: 60px;
+ ${mobile({ height: "50px" })}
+ `
+ const Wrapper=styled.div`
+ padding: 10px 20px;
+ display: flex;
+ align-items:center
+ margin-left:25px
+ justify-content: space-between;
+ ${mobile({ padding: "10px 0px" })}
+ `
+ const Input=styled.input`
+border:none;
+${mobile({ width: "50px" })}
+ `
+ const Left = styled.div`
+ flex:1
+ display:flex
+ align-items:center`
+
+ const Language=styled.span`
+ font-size:14px;
+ cursor: pointer;
+ ${mobile({ display: "none" })}
+ `
+ const SearchContainer=styled.div`
+ border: 1px solid lightgray;
+ display:flex;
+ `
+ const Center= styled.div`
+ text-align:right;
+ flex:1;
+ `
+  const Logo=styled.h1`
+  font-weight:bold
+  ${mobile({ fontSize: "24px" })}
+  `
+ const Right= styled.div`
+ display:flex;
+ align-items:center;
+ justify-content:flex-end;
+ flex:1
+ ${mobile({ flex: 2, justifyContent: "center" })}
+ `
+const MenuItem=styled.div`
+font-size:14px;
+cursor:pointer;
+margin-left:25px;
+${mobile({ fontSize: "12px", marginLeft: "10px" })}
+`
+const navbar = () => {
+  return (
+    <Container>
+      <Wrapper> 
+        <Left>
+          <Language>
+          EN
+          </Language>
+          <SearchContainer>
+           <Input/>
+           <Search style={{color:"gray",fontSize:16}}/>
+          </SearchContainer>
+      </Left>
+
+        <Center><Logo>Eea-com</Logo></Center>
+    <Right>
+    <Link to="../">
+  <MenuItem>Home</MenuItem>
+  </Link>
+  <Link to="../Product">
+  <MenuItem>Product</MenuItem>
+  </Link>
+  <Link to="../ProductList">
+  <MenuItem>Product List</MenuItem>
+  </Link>
+   <Link to="../Register">
+  <MenuItem>REGISTER</MenuItem>
+  </Link>
+  <Link to="../Login" >
+<MenuItem>SIGNIN</MenuItem>
+</Link>
+<Link to="../cart">
+<MenuItem>
+<Badge badgeContent={4} color="primary">
+  <ShoppingCartOutlined/>
+</Badge>
+</MenuItem>
+</Link>
+</Right>
+    </Wrapper>
+  </Container>
+  )
+};
+
+export default navbar
---
+++ src/data.js
@@ -0,0 +1,72 @@
+export const sliderItems = [{
+    id: 1,
+    img: "https://rukminim2.flixcart.com/image/714/857/k0e66q80/shirt/8/m/a/38-fw01-finivo-fashion-original-imafk7bqb6aatuqg.jpeg?q=50",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE GET FLAT 49% OFF FOR NEW ARRIVALS",
+    bg: "f5fafd",
+    cat: "women"
+}, {
+    id: 2,
+    img: "https://media.istockphoto.com/photos/window-shopping-picture-id867341470?k=20&m=867341470&s=612x612&w=0&h=p5v3hbbe-fLfT8Q4b77tgFhyiMx4p30aFFrIJpJpZ3U=",
+    title: "WINTER SALE",
+    desc: "DON'T COMPROMISE ON STYLE GET FLAT 49% OFF FOR NEW ARRIVALS",
+    bg: "f5f1ed",
+    cat: "coat"
+}, {
+    id: 3,
+    img: "https://rukminim1.flixcart.com/image/800/960/krz97rk0/dress/g/j/p/l-ftn-010-fashion-triangle-original-imag5nq8bfkzxqyb.jpeg?q=50",
+    title: "POPULAR SALE",
+    desc: "DON'T COMPROMISE ON STYLE GET FLAT 49% OFF FOR NEW ARRIVALS",
+    bg: "f5f0f4",
+    cat: "jeans"
+}]
+
+export const categories = [{
+        id: 1,
+        img: "https://images.bewakoof.com/uploads/grid/app/types-of-shirts-for-men-bewakoof-blog-1-1610963787.jpg",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://imgshopimages.lbb.in/catalog/product/1/_/1_278_739.jpg",
+        title: "LONGEWEAR LOVE",
+    }, {
+        id: 3,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkS656-oWABPWMeiN-gGbxy7CIrAvn6xHi6vDtQJVX7Ogv3z1pld7a4gZAkOrEYY3nJlQ&usqp=CAU",
+        title: "LIGHT JACKETS",
+    }
+]
+
+export const popularProducts = [{
+        id: 1,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+    },
+    {
+        id: 2,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRM5CZNf2WtgsFRuYqQZoPIiHUC3oeKe2eYfQ&usqp=CAU",
+    },
+    {
+        id: 3,
+        img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+    },
+    {
+        id: 4,
+        img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+    },
+    {
+        id: 5,
+        img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+    },
+    {
+        id: 6,
+        img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+    },
+    {
+        id: 7,
+        img: "https://rukminim1.flixcart.com/image/714/857/jlsc58w0/shirt/8/e/n/xl-partywear-n-t-fashion-original-imaf8u2vs5guqjsa.jpeg?q=50",
+    },
+    {
+        id: 8,
+        img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+]
